54d61791a58018dded5aa96844c89597ca315ecf
Nick <npavlov@conduktor.io>
2023-01-24 10:01:42 +0300

    Review fixes

--- zio-kafka-test-utils/src/main/scala/zio/kafka/KafkaTestUtils.scala
+++ zio-kafka-test-utils/src/main/scala/zio/kafka/KafkaTestUtils.scala
@@ -1,7 +1,7 @@
 package zio.kafka
 
 import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.clients.producer.{ProducerRecord, RecordMetadata}
+import org.apache.kafka.clients.producer.{ ProducerRecord, RecordMetadata }
 import zio._
 import zio.kafka.admin._
 import zio.kafka.consumer.Consumer.OffsetRetrieval
@@ -9,7 +9,7 @@ import zio.kafka.consumer._
 import zio.kafka.consumer.diagnostics.Diagnostics
 import zio.kafka.embedded.Kafka
 import zio.kafka.producer._
-import zio.kafka.serde.{Deserializer, Serde, Serializer}
+import zio.kafka.serde.{ Deserializer, Serde, Serializer }
 
 import java.io.File
 import java.nio.file.Paths
