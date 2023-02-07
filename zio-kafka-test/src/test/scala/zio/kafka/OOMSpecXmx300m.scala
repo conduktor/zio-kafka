56545e1ea1305092b30823c8697de9c2b00351bb
Guillaume Bécan <gbecan@conduktor.io>
2023-02-07 15:15:44 +0100

    Encapsulate errors from SslHelper.validateEndpoint in KafkaException (#153)

--- zio-kafka-test/src/test/scala/zio/kafka/OOMSpecXmx300m.scala
+++ zio-kafka-test/src/test/scala/zio/kafka/OOMSpecXmx300m.scala
@@ -1,10 +1,12 @@
 package zio.kafka
 
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.KafkaException
 import zio.kafka.consumer.{ Consumer, Subscription }
 import zio.kafka.embedded.Kafka
 import zio.kafka.producer.Producer
 import zio.kafka.serde.Serde
+import zio.test.Assertion._
 import zio.test.TestAspect._
 import zio.test._
 
@@ -22,12 +24,19 @@ object OOMSpecXmx300m extends ZIOSpecWithSslKafka {
                       topic <- randomTopic
                       _     <- Producer.produce(new ProducerRecord(topic, "boo", "baa"), Serde.string, Serde.string)
                     } yield ()).provideSomeLayer(KafkaTestUtils.producer).exit
-        } yield assertTrue(result.isFailure) &&
-          assertTrue(
-            result.toEither.left.map(_.getMessage()) == Left(
-              "Received an unexpected SSL packet from the server. Please ensure the client is properly configured with SSL enabled"
+        } yield assert(result)(
+          fails(
+            isSubtype[KafkaException](
+              hasField(
+                "cause message",
+                _.getCause.getMessage,
+                equalTo(
+                  "Received an unexpected SSL packet from the server. Please ensure the client is properly configured with SSL enabled"
+                )
+              )
             )
           )
+        )
       },
       test("consumer should fail with ssl check") {
         for {
@@ -35,24 +44,38 @@ object OOMSpecXmx300m extends ZIOSpecWithSslKafka {
                       topic <- randomTopic
                       _     <- Consumer.subscribe(Subscription.Topics(Set(topic)))
                     } yield ()).provideSomeLayer(KafkaTestUtils.consumer("test")).exit
-        } yield assertTrue(result.isFailure) &&
-          assertTrue(
-            result.toEither.left.map(_.getMessage()) == Left(
-              "Received an unexpected SSL packet from the server. Please ensure the client is properly configured with SSL enabled"
+        } yield assert(result)(
+          fails(
+            isSubtype[KafkaException](
+              hasField(
+                "cause message",
+                _.getCause.getMessage,
+                equalTo(
+                  "Received an unexpected SSL packet from the server. Please ensure the client is properly configured with SSL enabled"
+                )
+              )
             )
           )
+        )
       },
       test("admin client should fail with ssl check") {
         for {
           result <- (KafkaTestUtils.withAdmin { client =>
                       client.listTopics()
                     }).exit
-        } yield assertTrue(result.isFailure) &&
-          assertTrue(
-            result.toEither.left.map(_.getMessage()) == Left(
-              "Received an unexpected SSL packet from the server. Please ensure the client is properly configured with SSL enabled"
+        } yield assert(result)(
+          fails(
+            isSubtype[KafkaException](
+              hasField(
+                "cause message",
+                _.getCause.getMessage,
+                equalTo(
+                  "Received an unexpected SSL packet from the server. Please ensure the client is properly configured with SSL enabled"
+                )
+              )
             )
           )
+        )
       }
     ) @@ withLiveClock @@ sequential
 }
